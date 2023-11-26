6cf302b7c05391cb98e5ebfabeccc0b2de4a3544
Rajeewa Asmadala <110443590+rajeewa-as@users.noreply.github.com>
2023-11-26 20:00:12 +0530

    Add project files.

---
+++ BlazorMovie/Services/TMDBClient.cs
@@ -0,0 +1,33 @@
+﻿using BlazorMovie.Models;
+using System.Net.Http.Json;
+
+namespace BlazorMovie.Services;
+
+public class TMDBClient
+{
+    private readonly HttpClient _httpClient;
+
+    public TMDBClient(HttpClient httpClient, IConfiguration configuration)
+    {
+        _httpClient = httpClient;
+
+        _httpClient.BaseAddress = new Uri("https://api.themoviedb.org/3/");
+        _httpClient.DefaultRequestHeaders.Accept.Add(new("application/json"));
+
+        string apiKey = configuration["TMDBKey"] ?? throw new ArgumentNullException(configuration["TMDBKey"]);
+        _httpClient.DefaultRequestHeaders.Authorization = new("Bearer", apiKey);
+    }
+
+    public Task<PopularMoviePagedResponse?> GetPopularMoviesAsync(int page = 1)
+    {
+        if (page < 1) page = 1;
+        if (page > 500) page = 500;
+
+        return _httpClient.GetFromJsonAsync<PopularMoviePagedResponse>($"movie/popular?page={page}");
+    }
+
+    public Task<MovieDetailsResponse?> GetMovieDetailsAsync(int id)
+    {
+        return _httpClient.GetFromJsonAsync<MovieDetailsResponse>($"movie/{id}");
+    }
+}
